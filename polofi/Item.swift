d26a333690338851240864c112758028c2fcd3c1
Amelia Citra <ameliacitradewi@gmail.com>
2026-04-02 11:24:50 +0700

    Initial Commit

---
+++ polofi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  polofi
+//
+//  Created by Amelia Citra on 02/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
