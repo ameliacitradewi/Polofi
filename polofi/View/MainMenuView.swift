753c88afffa21b1ed1d73209be4bce6366fc59c4
Amelia Citra <ameliacitradewi@gmail.com>
2026-06-12 00:09:19 +0700

    changes UI Mainmenu

--- polofi/View/MainMenuView.swift
+++ polofi/View/MainMenuView.swift
@@ -11,53 +11,86 @@ struct MainMenuView: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                // background
+                LinearGradient(colors: [Color.color1, Color.color2], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
                 
-                VStack {
-                    VStack (alignment: .trailing) {
+                VStack (spacing: 20) {
+                    VStack(alignment: .trailing) {
                         Text("Focus Timer")
                             .font(.largeTitle.bold())
-                        
+
                         Text("""
                             Stay locked in. Work in focused sprints,
                             rest with intention, and watch your productivity compound.
                             """)
-                        
+                        .fontWeight(.semibold)
+
                         Spacer()
                     }
                     .padding(.top, 30)
+                    .padding(.horizontal, 16)
                     .multilineTextAlignment(.trailing)
-                    .frame(maxWidth: .infinity)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
                     .frame(height: geo.size.height * 0.7)
                     .background(alignment: .leading) {
                         Image("clock2")
                             .resizable()
-                            .scaledToFit()
+                            .scaledToFill()
                     }
-                    .clipped()
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 28, style: .continuous)
+                    )
+//                    .overlay {
+//                        RoundedRectangle(cornerRadius: 28, style: .continuous)
+//                            .stroke(Color.white.opacity(0.35), lineWidth: 1.5)
+//                    }
+                    .shadow(
+                        color: Color.black.opacity(0.25),
+                        radius: 4,
+                        x: 0,
+                        y: 4
+                    )
                     
-                    VStack (alignment: .trailing) {
+                    
+                    VStack(alignment: .trailing) {
                         Text("Music Player")
                             .font(.largeTitle.bold())
-                        
+
                         Text("""
                             Pre loaded lo-fi tracks,
                             ready to play.
                             """)
+                        .fontWeight(.semibold)
                     }
-                    .frame(maxWidth: .infinity)
-                    .frame(height: geo.size.height * 0.3)
+                    .padding(.horizontal, 16)
+                    .multilineTextAlignment(.trailing)
+                    .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                    .frame(height: geo.size.height * 0.2)
                     .background(alignment: .leading) {
                         Image("headphone")
                             .resizable()
-                            .scaledToFit()
+                            .scaledToFill()
                     }
-                    .clipped()
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 28, style: .continuous)
+                    )
+//                    .overlay {
+//                        RoundedRectangle(cornerRadius: 28, style: .continuous)
+//                            .stroke(Color.white.opacity(0.35), lineWidth: 1.5)
+//                    }
+                    .shadow(
+                        color: Color.black.opacity(0.25),
+                        radius: 4,
+                        x: 0,
+                        y: 4
+                    )
                     
                 }
+                .foregroundColor(Color.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 10)
                 .frame(width: .infinity, height: geo.size.height)
             }
-            .padding(.horizontal, 10)
         }
     }
 }
